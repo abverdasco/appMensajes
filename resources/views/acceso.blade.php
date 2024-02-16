008af870f4a0a481d187660a567d408c888d1786
Ramiro <rmmfree@gmail.com>
2024-02-16 09:35:51 +0100

    Añado vistas

---
+++ resources/views/acceso.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <h1>Acceso a la app de mensajería</h1>
+    <form action="{{ url('login') }}" method="post">
+        <label for="nombre">Nombre de usuario</label>
+        <br>
+        <input type="text" name="nombre" />
+        <br>
+        <input type="submit" value="Acceder" />
+    </form>
+
+</div>
