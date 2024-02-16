fb5aa3f04002b03ee23ff198ee0cbab62987f428
Ramiro <rmmfree@gmail.com>
2024-02-16 09:51:12 +0100

    Añado validación csrf

--- resources/views/acceso.blade.php
+++ resources/views/acceso.blade.php
@@ -1,6 +1,7 @@
 <div>
     <h1>Acceso a la app de mensajería</h1>
     <form action="{{ url('login') }}" method="post">
+        @csrf
         <label for="nombre">Nombre de usuario</label>
         <br>
         <input type="text" name="nombre" />
