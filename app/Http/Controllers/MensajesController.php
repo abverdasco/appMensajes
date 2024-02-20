15af01278a1febc5417028f4fdca17f791c7f915
Ramiro <rmmfree@gmail.com>
2024-02-20 09:51:20 +0100

    Guardar mensajes en el servidor

--- app/Http/Controllers/MensajesController.php
+++ app/Http/Controllers/MensajesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Mensaje;
+use Illuminate\Support\Facades\Session;
 
 class MensajesController extends Controller
 {
@@ -16,7 +18,28 @@ class MensajesController extends Controller
     }
 
     // El usuario envía un mensaje por AJAX:
-    public function enviarMensaje() {
+    public function enviarMensaje(Request $request) {
         
+        // Obtenemos el mensaje recibido
+        $texto = $request->get('mensajeEnvio');
+        
+        // Creamos una variable vacía de tipo Mensaje
+        $mensaje = new Mensaje();
+
+        // Asignamos la variable para el campo texto
+        $mensaje->texto = $texto;
+
+        $session = Session();
+        $usuario = $session->get('nombre');
+
+        // Asignamos la variable para el campo usuario
+        $mensaje->usuario = $usuario;
+
+        // Guardamos el registro en la base de datos
+        $mensaje->save();
+
+        echo 'Mensaje enviado y guardado';
+
+
     }
 }
